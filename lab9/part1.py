ccf34ef5764517cf03472c43d58d919057ed2798
Amanda Baran <amandabaran9@gmail.com>
2023-03-11 15:28:58 -0500

    part 1

---
+++ lab9/part1.py
@@ -0,0 +1,108 @@
+import sys
+import cv2 as cv
+import numpy as np
+        
+# def load_image(argv):
+#     default_file = 'smarties.png'
+#     filename = argv[0] if len(argv) > 0 else default_file
+#     # Loads an image
+#     src = cv.imread(cv.samples.findFile(filename), cv.IMREAD_COLOR)
+#     # Check if image is loaded fine
+#     if src is None:
+#         print ('Error opening image!')
+#         print ('Usage: hough_circle.py [image_name -- default ' + default_file + '] \n')
+#         return -1
+#     return src
+
+class Cam:
+    
+    def __init__(self, raspberrypi = False):
+        
+        self.raspberrypi = raspberrypi
+       
+        if raspberrypi:
+            # Only works on raspberry pi
+            from picamera2 import Picamera2 
+            self.cam = Picamera2()
+            self.cam.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+            self.cam.start()
+        else:
+            self.cam = cv.VideoCapture(0)
+    
+    def stop(self):
+        if not self.raspberrypi:
+            self.cam.release()
+        cv.destroyAllWindows()
+
+    def load_image(self):
+        if self.raspberrypi:
+            return self.cam.capture_array()
+        else:
+            _, frame = self.cam.read()
+            return frame
+        
+
+    
+try:
+    
+    cam = Cam()
+    
+    while True:
+        
+        # Close frame window by typing q
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+        # src = load_image(argv)
+        frame = cam.load_image()
+        
+        # It converts the BGR color space of image to HSV color space
+        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+        
+        # Threshold of blue in HSV space
+        lower_blue = np.array([60, 35, 140])
+        upper_blue = np.array([180, 255, 255])
+        
+        # preparing the mask to overlay
+        mask = cv.inRange(hsv, lower_blue, upper_blue)
+        
+        # The black region in the mask has the value of 0,
+        # so when multiplied with original image removes all non-blue regions
+        result = cv.bitwise_and(frame, frame, mask = mask)
+    
+        # cv.imshow('frame', frame)
+        # cv.imshow('mask', mask)
+        cv.imshow('result', result)
+        
+                
+        # covert to grayscale and apply blur to reduce noise
+        gray = cv.cvtColor(result, cv.COLOR_BGR2GRAY)
+        gray = cv.medianBlur(gray, 5)
+        
+        
+        # Hough cirlce transform
+        rows = gray.shape[0]
+        circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
+                                param1=100, param2=30,
+                                minRadius=1, maxRadius=100)
+        
+        
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            for i in circles[0, :]:
+                center = (i[0], i[1])
+                # circle center
+                cv.circle(gray, center, 1, (0, 100, 100), 3)
+                # circle outline
+                radius = i[2]
+                cv.circle(gray, center, radius, (255, 0, 255), 3)
+        
+        # print(circles)
+        cv.imshow('detected circles', gray)
+        
+    # When everything done, release the capture
+    cam.stop()
+    
+except KeyboardInterrupt:
+    cam.stop()
+
---
+++ midterm/midterm.py
@@ -0,0 +1,82 @@
+import numpy as np
+import apis
+import time
+import sys
+
+factor = 3.0
+
+def elipse(a, b, t, init):
+    return np.array([init[0] + a - a * np.cos(t / factor), init[1] + b * np.sin(t / factor)])
+
+if __name__ == "__main__":
+
+    IP_ADDRESS = '192.168.0.207'
+
+    robot = apis.Robot(IP_ADDRESS)
+  
+    duck = np.array([0.9794962406158447, 1.392021656036377])
+    box_width = .65
+
+    print("Starting")
+    try:
+        # hostname = socket.gethostname()
+        # ip_addr = socket.gethostbyname(hostname)
+        clientAddress = "192.168.0.25"
+        optitrackServerAddress = "192.168.0.4"
+        robot_id = 207
+        
+        # Start up the streaming client now that the callbacks are set up.
+        # This will run perpetually, and operate on a separate thread.
+
+        position = apis.Position(clientAddress, optitrackServerAddress, robot_id)
+
+        K1 = 2000#100
+        K2 = 2500
+        
+        start = time.time()
+
+        init, angle = position.get()
+
+        sign = -1
+
+        if init[0] <= duck[0]:
+            sign = 1
+
+        a = sign * apis.dist(init, duck) / 2.0
+
+        print(a)
+
+        b = box_width
+        
+        while True:
+
+            x_t, angle = position.get()
+
+            if time.time() - start <= 2 * np.pi * factor:
+                x_d = elipse(a, b, time.time() - start, init)
+            else:
+                x_d = init
+            
+            dist, desiredAngle = apis.dist_and_angle(x_d, x_t)
+
+            v = K1 * dist
+            angleDiff = apis.angle_diff(desiredAngle, angle)
+            omega = K2 * angleDiff
+
+            u = np.array([v - omega, v + omega])
+            u[u > 1500.] = 1500.
+            u[u < -1500.] = -1500.
+            
+            robot.set_motor(u[0], u[0], u[1], u[1])
+            time.sleep(0.1)
+
+    except KeyboardInterrupt:
+        print("Shutting down")
+        robot.stop_motor()
+        time.sleep(1)
+        robot.stop_motor()
+        pass
+    
+    robot.stop_motor()
+    robot.shutdown()
+    print("Done")
