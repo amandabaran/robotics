a28005d8ae03870e4b51371f374b5301ec7b8123
Amanda Baran <amandabaran9@gmail.com>
2023-04-05 11:50:19 -0400

    finished lab6 - robot tracks ball

--- lab6/part2.py
+++ lab6/part2.py
@@ -3,20 +3,34 @@ import numpy as np
 import Motor
 import time 
 
+H_low = 0
+H_high = 46
+S_low= 207
+S_high = 255
+V_low= 0
+V_high = 255
+
+# https://blog.electroica.com/hsv-trackbar-opencv-python/
+def callback(x):
+    global H_low,H_high,S_low,S_high,V_low,V_high
+    #assign trackbar position value to H,S,V High and low variable
+    H_low = cv2.getTrackbarPos('low H','controls')
+    H_high = cv2.getTrackbarPos('high H','controls')
+    S_low = cv2.getTrackbarPos('low S','controls')
+    S_high = cv2.getTrackbarPos('high S','controls')
+    V_low = cv2.getTrackbarPos('low V','controls')
+    V_high = cv2.getTrackbarPos('high V','controls')
+    
 class Robot:
     def __init__(self):
         self.PWM = Motor.Motor()
-        self.ultra=Ultrasonic.Ultrasonic() 
 
-    def move(self, speed):
-        self.PWM.setMotorModel(-speed,-speed,speed,speed)
+    def move(self, left_speed, right_speed):
+        self.PWM.setMotorModel(left_speed, left_speed, right_speed, right_speed)
 
     def stop(self):
         # Stop
         self.PWM.setMotorModel(0,0,0,0)
-
-    def sense(self):
-        return self.ultra.get_distance()
 
 
 class Cam:
@@ -29,7 +43,7 @@ class Cam:
             # Only works on raspberry pi
             from picamera2 import Picamera2
             self.cam = Picamera2()
-            self.cam.configure(Picamera2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+            self.cam.configure(self.cam.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
             self.cam.start()
         else:
             self.cam = cv2.VideoCapture(0)
@@ -41,32 +55,59 @@ class Cam:
             _, frame = self.cam.read()
             return frame
         
-    def find_blob(self, lower, upper):
-        # Read image
-        im = self.load_image()
+    def stop(self):
+        if not self.raspberrypi:
+            self.cam.release()
+        cv2.destroyAllWindows()
+    
+    def find_blobs(self,lower_val, upper_val):
+        # get image from robot camera
+        frame = self.load_image()     
+        # converts the BGR color space of image to HSV color space
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  
+        
+        # preparing the mask to overlay
+        mask = cv2.inRange(hsv, lower_val, upper_val)
+        
+        # The black region in the mask has the value of 0,
+        # so when multiplied with original image removes all non-blue regions
+        result = cv2.bitwise_and(frame, frame, mask = mask)
+        
+        # cv2.imshow('frame', frame)
+        # cv2.imshow('mask', mask)
+        # cv2.imshow('result', result)
+        
+        result = cv2.bitwise_not(result)
+        
+        # covert to grayscale and apply blur to reduce noise
+        gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+        gray = cv2.medianBlur(gray, 11)
         
         # Setup SimpleBlobDetector parameters.
         params = cv2.SimpleBlobDetector_Params()
         
         # Change thresholds
-        params.minThreshold = 10;
-        params.maxThreshold = 200;
+        params.minThreshold = 100;
+        params.maxThreshold = 255;
         
         # Filter by Area.
         params.filterByArea = True
-        params.minArea = 1500
+        params.minArea = 1000
+        params.maxArea = 2**64
         
         # Filter by Circularity
-        params.filterByCircularity = True
+        params.filterByCircularity = False
         params.minCircularity = 0.1
         
         # Filter by Convexity
-        params.filterByConvexity = True
+        params.filterByConvexity = False
         params.minConvexity = 0.87
         
         # Filter by Inertia
-        params.filterByInertia = True
+        params.filterByInertia = False
         params.minInertiaRatio = 0.01
+        
+        detector = None
         
         # Create a detector with the parameters
         ver = (cv2.__version__).split('.')
@@ -76,89 +117,83 @@ class Cam:
             detector = cv2.SimpleBlobDetector_create(params)
         
         # Detect blobs.
-        keypoints = detector.detect(im)
+        keypoints = detector.detect(gray)
         
         # Draw detected blobs as red circles.
         # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-        im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        im_with_keypoints = cv2.drawKeypoints(gray, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         
-        # Show keypoints
-        cv2.imshow("Keypoints", im_with_keypoints)
-        cv2.waitKey(0)
+        blobs = []
+        for k in keypoints:
+            blobs.append([k.pt[0], k.pt[1], k.size])
         
-    def find_ball(self, lower_val, upper_val):
-        frame = self.load_image()
+        if len(blobs) != 0:
+            return np.array([blobs]), im_with_keypoints
         
-        # converts the BGR color space of image to HSV color space
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        
-        # preparing the mask to overlay
-        mask = cv2.inRange(hsv, lower_val, upper_val)
-        
-        # The black region in the mask has the value of 0,
-        # so when multiplied with original image removes all non-blue regions
-        result = cv2.bitwise_and(frame, frame, mask = mask)
-    
-        # cv2.imshow('frame', frame)
-        # cv2.imshow('mask', mask)
-        # cv2.imshow('result', result)
-        
-        # covert to grayscale and apply blur to reduce noise
-        gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
-        gray = cv2.medianBlur(gray, 5)
+        return None, im_with_keypoints
         
         
-        # Hough cirlce transform
-        rows = gray.shape[0]
-        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows / 8,
-                                param1=100, param2=30,
-                                minRadius=1, maxRadius=100)
+    def find_ball(self, lower_val, upper_val):    
+        blobs, gray = self.find_blobs(lower_val, upper_val)
         
-        if (circles is None) or (len(circles) < 1):
-            return None
-
-        size = 0
-        index = 0
-        circles = circles[0]
-        
-        # Find biggest circle and store index
-        for j in range(len(circles)):
-            if circles[j][2] > size:
-                index = j
-                size = circles[j][2]
-                  
-        return circles[index]
-        
-    def stop(self):
-        if not self.raspberrypi:
-            self.cam.release()
-        cv2.destroyAllWindows()
-    
+        if blobs is None:
+            return (None, gray)
+        else:
+            blobs = np.uint16(np.around(blobs))
+            
+            radius = -10000
+            ball = None
+            
+            for b in blobs[0, :]:
+                r = b[2]
+                if r > radius:
+                    radius = r
+                    ball = b
+            return (ball, gray)
+                    
 try:
     
-    cam = Cam()
+    cam = Cam(True) #use raspberry pi
     robot = Robot()
     
+    # use sliders to control color filter
+    cv2.namedWindow('controls', cv2.WINDOW_NORMAL)
+    cv2.createTrackbar('low H','controls',0,179,callback)
+    cv2.createTrackbar('high H','controls',179,179,callback)
+    cv2.createTrackbar('low S','controls',0,255,callback)
+    cv2.createTrackbar('high S','controls',255,255,callback)
+    cv2.createTrackbar('low V','controls',0,255,callback)
+    cv2.createTrackbar('high V','controls',255,255,callback)
+    
+    found_ball = False
+
     while True:
-      
-        # Threshold of blue in HSV space
-        lower_blue = np.array([60, 35, 140])
-        upper_blue = np.array([180, 255, 255])
+    
+        ball, gray = cam.find_ball(np.array([H_low, S_low, V_low]), np.array([H_high, S_high, V_high]))
+          
+        if ball is None:
+            found_ball = False
+        else :
+            found_ball = True
         
-        # May need to increase range for detection of ball in lab
-        lower_green = np.array([36, 0 , 0])
-        upper_green = np.array([86, 255, 255])
+        print(found_ball, "\n\n")
         
-        ball = cam.find_ball(lower_green, upper_green)
-        
-        if ball is not None:
-            found = True
-            print("Found ball\n")
-            robot.stop()
-        
-        else:
-            robot.move(1200)
+        if found_ball is False:
+            robot.move(-1000, 1000)
             
+        else:
+            # Below is helped from depaul due to my health abseences
+            # move left if ball[0] > 320
+            # move right if ball[0] < 320
+            k1 = 2
+            k2 = 900
+            angle = 320 - ball[0]
+            robot.move(k2-k1*angle, k2+k1*angle)
+        
+        cv2.imshow('frame', gray)   
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
         time.sleep(0.1)
             
     
